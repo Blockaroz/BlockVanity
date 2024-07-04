c9957cdc6024924c69a8b2252d0286d6314aa5af
Blockaroz <lbpk.123fan@gmail.com>
2024-07-04 03:05:24 -0400

    update to July 2024

---
+++ Content/Items/Vanity/Myrtle/MyrtleClothes.cs
@@ -0,0 +1,28 @@
+﻿using BlockVanity.Common.Utilities;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BlockVanity.Content.Items.Vanity.Myrtle;
+
+[AutoloadEquip(EquipType.Head)]
+public class PlumeriaPin : VanityItem
+{
+    public PlumeriaPin() : base(ItemRarityID.Green) { }
+
+    public override void SetStaticDefaults()
+    {
+        ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true;
+    }
+}
+
+[AutoloadEquip(EquipType.Body)]
+public class MyrtleDress : VanityItem
+{
+    public MyrtleDress() : base(ItemRarityID.Green) { }
+}
+
+[AutoloadEquip(EquipType.Legs)]
+public class MyrtleSandals : VanityItem
+{
+    public MyrtleSandals() : base(ItemRarityID.Green) { }
+}
